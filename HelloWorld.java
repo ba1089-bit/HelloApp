5335fc53495f638014c8f40010442cc7fae0a6dd
ashish <ba1089@srmist.edu.in>
2026-03-25 13:37:13 +0530

    UC4: Display multiple names using command-line arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,17 @@
+// UC4: Display multiple names from command-line
+
 public class HelloWorld {
     public static void main(String[] args) {
-        String name;
 
-        if (args.length > 0) {
-            name = args[0];
-        } else {
-            name = "World";
+        // If no arguments → default
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } 
+        else {
+            // Loop through all names
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
         }
-
-        System.out.println("Hello " + name);
     }
 }
